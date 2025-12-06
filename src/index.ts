ccf2e0c3278909f76aa84661ee07bbb2f6f913a1
Muhammad Raheel <raheeluafbrw@gmail.com>
2025-12-07 00:55:42 +0500

    feat: streaming types

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { sqlQueryAgent } from "./agents/agent-factory.js";
 import { drawGraph } from "./utils/drawGraph.js";
 import { database } from "./db/datasource.js";
 import { displayStream } from "./utils/displayStream.js";
+import { HumanMessage } from "langchain";
 
 await drawGraph(sqlQueryAgent, "sql-query-agent-graph");
 
@@ -18,16 +19,28 @@ await drawGraph(sqlQueryAgent, "sql-query-agent-graph");
 
 // console.log("AGENT:", response);
 
+const humanMessage = new HumanMessage(
+  "Tell me about the most customer Frank please"
+);
+
 const response = await sqlQueryAgent.stream(
   {
-    messages: "Gimme the best songs playlist",
+    // messages: humanMessage, better way to pass message, best type safety
+    messages: {
+      role: "human",
+      content: "Tell me about the most customer Frank Harris please",
+    }, // another way to pass message
   },
   {
-    streamMode: "values",
+    streamMode: "messages", // messages | value | custom
     context: {
       db: database,
     },
   }
 );
+// console.log("RESPONSE:::::", response);
+// displayStream(response);
 
-displayStream(response);
+for await (const [message, metadata] of response as any) {
+  console.log(`[${metadata.langgraph_node}]: ${message.content}`);
+}
